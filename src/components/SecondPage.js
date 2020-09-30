d4b42d2979263e18a2b8674b5a4e9342e7e09283
[Eligreg Lopez] <[eligregl@gmail.com]>
2020-09-29 22:54:46 -0500

    segundo paso hecho

--- src/components/SecondPage.js
+++ src/components/SecondPage.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
+import './SecondPage.css'
 
 function SecondPage(props) {
   const { goToSecondPage, goToFirstPage, Nombre } = props
   return (
-    <div>
-     <p>Hola { Nombre } Bienvenido(a) a Globant.</p>
-     <button onClick={goToSecondPage}>Next</button>
-     <button onClick={goToFirstPage}>Previous</button>
+    <div className="container">
+      <p> <span className="glober">{ Nombre }</span> already belongs to a studio, has a seniority and probably already works on a specific project. To understand this better</p>
+      <p className="emphasis">Let's imagine!</p>
+      <p>First, we need to create our <span className="emphasis">fictional character!</span></p>
+      <div className="buttons">
+        <button onClick={goToSecondPage}>Next</button>
+        <button onClick={goToFirstPage}>Previous</button>
+      </div>
     </div>
   );
 }
